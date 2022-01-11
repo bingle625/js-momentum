585ec5ebef9680e6f43dd8db1a9bf7699d4af469
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-11 13:01:49 +0900

    array안에 접근하기,  push로 추가하기

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,13 +1,5 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
-
-const daysOfWeek =[mon, tue , wed,  thu,  fri,  sat,  sun];
-const nonsense = [1 ,"hi", true , undefined , null , false, mon];
-
-console.log(daysOfWeek , nonsense);
-
+const daysOfWeek =["mon", "tue" , "wed",  "thu",  "fri",  "sat"];
+console.log(daysOfWeek);
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
