57d3421bc1420dc0ea5e959446c971d73850eddf
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-11 12:53:29 +0900

    배열 이용해보기

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,2 +1,13 @@
-const amIFat = undefined;
-console.log(amIFat);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek =[mon, tue , wed,  thu,  fri,  sat,  sun];
+const nonsense = [1 ,"hi", true , undefined , null , false, mon];
+
+console.log(daysOfWeek , nonsense);
+
