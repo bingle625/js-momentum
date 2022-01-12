3c14f8c219dfb32eb91a76787630682123892bbd
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-12 19:01:18 +0900

    prompt 창과 함수 활용)

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,24 +1,3 @@
-const calculator = {
-    plus: function (a,b) {
-        return a+b;
-    },
-    minus: function (a,b) {
-        return a-b;
-    },
-    times: function (a,b) {
-        return a*b;
-    },
-    divide: function (a,b) {
-        return a / b;
-    },
-    power: function (a,b) {
-        return a ** b;
-    },
-}
+const age = parseInt(prompt("몇살이십니까?"));
 
-console.log(calculator.plus(2,3));
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.power(minusResult,-3);
-
-console.log(plusResult,minusResult,timesResult);
+console.log(isNaN(age));
