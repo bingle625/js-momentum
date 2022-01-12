d601a9f7bfb78d4fcc2f5d17d41de5bbb368c7bd
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-12 18:38:18 +0900

    계산기 alert

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,8 +1,19 @@
-const player = {
-    name : "seongJae",
-    sayHello : function(nameOf){
-        console.log("Hello " + nameOf + " nice to meet you!" );
+const calculator = {
+    plus: function (a,b) {
+        alert(a+b);
+    },
+    minus: function (a,b) {
+        alert(a-b);
+    },
+    times: function (a,b) {
+        alert(a*b);
+    },
+    divide: function (a,b) {
+        alert(a / b);
+    },
+    power: function (a,b) {
+        alert(a ** b);
     },
 }
 
-player.sayHello("seongJae");
+calculator.power(2,3);
