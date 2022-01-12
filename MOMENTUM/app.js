78808a50aeced587cc735a21ab02ddba1f4f7a8b
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-12 18:46:15 +0900

    calculator return 활용

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,19 +1,24 @@
 const calculator = {
     plus: function (a,b) {
-        alert(a+b);
+        return a+b;
     },
     minus: function (a,b) {
-        alert(a-b);
+        return a-b;
     },
     times: function (a,b) {
-        alert(a*b);
+        return a*b;
     },
     divide: function (a,b) {
-        alert(a / b);
+        return a / b;
     },
     power: function (a,b) {
-        alert(a ** b);
+        return a ** b;
     },
 }
 
-calculator.power(2,3);
+console.log(calculator.plus(2,3));
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.power(minusResult,-3);
+
+console.log(plusResult,minusResult,timesResult);
