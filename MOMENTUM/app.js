d8272c92679f42e622efe5bdebc78409678016c4
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-14 00:59:02 +0900

    mdn 문서에서 window와 관련된 찾아 이벤트 사용

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -11,4 +11,34 @@ function handleTitleClick() {
     }
 }
 
+function handleTitleEnter() {
+    
+    Title.innerText = "The mouse is here!"
+}
+
+function handleTitleLeave() {
+    Title.innerText = "The mouse is gone!"
+    togle = !togle; 
+    if(togle){
+        Title.style.color = "blue";
+    }else{
+        Title.style.color = "black";
+    }
+}
+
+function handleResize() {
+    Title.innerText= "You just resized";
+    document.body.style.backgroundColor = "black";
+}
+
+
+function handleRightClick() {
+    Title.innerText= "That was a Right Clcik";
+}
+
+console.log(window);
 Title.addEventListener("click", handleTitleClick);
+Title.addEventListener("mouseenter",handleTitleEnter);
+Title.addEventListener("mouseleave",handleTitleLeave);
+window.addEventListener('resize', handleResize);
+window.addEventListener('contextmenu', handleRightClick);
