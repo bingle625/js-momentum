ca9a033c01a37dd03af7d499e4d064651a30caa5
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-14 12:18:33 +0900

    누르면 색이 바뀌는 web

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,7 +1,20 @@
 const title = document.querySelector("div.Hello h1");
 
 function handleTitleClick(){
-  title.className = "active";
+
+  const clickedClass = "active";
+  
+  // if(title.classList.contains(clickedClass)){
+
+  //   title.classList.remove(clickedClass);
+
+  // }
+  // else{
+  //   title.classList.add(clickedClass);
+  // }
+  // 위 코드를 한줄로 만들면
+
+  title.classList.toggle(clickedClass);
 }
 
 title.addEventListener("click",handleTitleClick);
