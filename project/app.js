324b1fb35e6aa7715043b50bb2e4c6fbdaa2d74a
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-17 23:38:39 +0900

    localstorage를 통해 사용자 상태 이용하기

--- project/app.js
+++ project/app.js
@@ -3,16 +3,35 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     // greeting.innerText = "Hello " + username; 아래와 같은 역할 수행함.
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+
 
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(name){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${name}`;
+
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUsername);
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+    console.log("form");
+}else{
+    // show the greeting
+    paintGreetings(savedUsername);
+}
