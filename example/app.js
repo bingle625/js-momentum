dde300d773f342b5e76bbf93014ae0bbcfe29184
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-18 15:25:43 +0900

    <example> 랜덤으로 숫자를 맞혀보는 프로젝트

--- example/app.js
+++ example/app.js
@@ -1,36 +1,38 @@
-const body = document.body;
-// const BIG_SCREEN ="bigScreen";
-// const MEDIUM_SCREEN = "mediumScreen";
-// const SMALL_SCREEN = "smallScreen";
+const printForm = document.querySelector("#print-form");
+const rangeInput = document.querySelector("h2 #range-input");
+const userInput = printForm.querySelector("#user-input");
+const message1 = document.querySelector("#result #message1");
+const message2 = document.querySelector("#result #message2");
 
-// const width = window.innerWidth;
-// if(width>1000){
-//     body.classList.add(BIG_SCREEN);
-//     body.classList.remove(MEDIUM_SCREEN);
-// }else if(width <= 1140 && width >= 700){
-//     body.classList.add(MEDIUM_SCREEN);
-//     body.classList.remove(SMALL_SCREEN);
-// } else{
-//     body.classList.remove(MEDIUM_SCREEN);
-//     body.classList.add(SMALL_SCREEN);
-// }
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
+  }
 
+function onSubmit(event){
+    event.preventDefault();
+    const rightLimit = rangeInput.value;
+    const chosen = parseInt(userInput.value);
+    const machineChosen = getRandomIntInclusive(0,rightLimit);
 
-function handleResize(){
-    const originalSize = window.screen.width;
-    const windowSize =window.innerWidth;
-    const percentage = windowSize/originalSize * 100;
-    // console.log(originalSize);
-    // console.log(windowSize);
-    // console.log(percentage);
-    if(percentage > 80){
-        body.className = "yello";
-    }else if(percentage > 60){
-        body.className = "purple";
-    }else{
-        body.className = "blue"
+    message1.innerHTML =`You chose: ${chosen}, the machine chose: ${machineChosen}`;
+    
+    if(chosen === machineChosen){
+        //맞았을 때
+        message2.innerHTML = `You Won!`
+        
     }
+    else if(chosen > rightLimit){
+        message2.innerHTML=`Impossible!`
+    }
+    else {
+        //틀렸을 때
+        message2.innerHTML = `You Lose!`
+    }
+
 }
 
-console.dir(window);
-window.addEventListener("resize",handleResize );
+printForm.addEventListener("submit",onSubmit);
+
+console.dir(userInput);
